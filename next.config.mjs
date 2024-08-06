9e8cf556dd16b85f78929fc7a7d6692e20c7dd6e
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-08-05 23:54:03 -0400

    Ensure correct build and deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   swcMinify: true,
+  distDir: 'dist', // This will change the output directory to 'dist'
 };
 
 export default nextConfig;
