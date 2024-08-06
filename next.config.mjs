30337279918a93e50207bca604b959a2c88a96a7
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-08-06 00:03:01 -0400

    Ensure correct build and deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Enable static export
   reactStrictMode: true, // Enables React Strict Mode
   swcMinify: true, // Enables the SWC compiler for minification
   distDir: 'dist', // Change the output directory to 'dist'
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
 };
 
 export default nextConfig;
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   reactStrictMode: true, // Enables React Strict Mode
+//   swcMinify: true, // Enables the SWC compiler for minification
+// };
+
+// export default nextConfig;
