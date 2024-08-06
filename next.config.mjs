ec324e8f1df4d7de54a416b12a29ac2600b1900d
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-08-05 21:53:55 -0400

    tesing render

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
   reactStrictMode: true, // Enables React Strict Mode
   swcMinify: true, // Enables the SWC compiler for minification
+  distDir: 'dist', // Change the output directory to 'dist'
 };
 
 export default nextConfig;
